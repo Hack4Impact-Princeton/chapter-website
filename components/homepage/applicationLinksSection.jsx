43720de1d764848882b0b2aab2f157a34aeb255f
EmilioC1 <ec2736@princeton.edu>
2023-08-02 12:28:47 -0400

    .gitignore and removed the outreach team from the recruiting section

--- components/homepage/applicationLinksSection.jsx
+++ components/homepage/applicationLinksSection.jsx
@@ -26,7 +26,7 @@ const ApplicationLinksSection = () => (
                         Developers will complete a 7-session bootcamp this fall to ensure that everyone is starting from an appropriate skill baseline. So underclassmen, don’t think inexperience should stop you from applying! The bootcamp will cover aspects of both front and back-end engineering with the MERN tech stack. Completion of the bootcamp means developers will be equipped to work on projects this spring.
                     </p>
                 </Col>
-                <Col className="p-4">
+                {/* <Col className="p-4">
                     <img className="text-center icon" src="/images/liaison-icon.svg" alt="Liaison Icon" />
                     <h3 className="text-center title">Nonprofit Liaison</h3>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSfp-NaUthvFcaKfrrk9NioM9dWdBQY6MeCkE1juvV6H-RCSTg/viewform" target="_blank">
@@ -35,7 +35,7 @@ const ApplicationLinksSection = () => (
                     <p className="text-justify">
                         The nonprofit outreach team works on sourcing projects for Hack4Impact. Coordinators will learn how to pitch Hack4Impact’s services and discern what issues are and are not suitable for a H4I project. One of H4I Princeton’s goals is also to establish a working collaboration with the Pace Center, so outreach coordinators may work with Pace to set up civic service internships to develop Princeton’s relationship with community partners. 
                     </p>
-                </Col>
+                </Col> */}
             </Row>
         </div>
     </Container>
