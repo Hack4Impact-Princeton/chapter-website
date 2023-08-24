d57b9cf40ebea5c5378870c9f23331cbdbde5614
jessica6080 <jyan6080@gmail.com>
2023-08-24 13:51:38 -0500

    add apply section and misc components

---
+++ components/ContentBlock.jsx
@@ -0,0 +1,13 @@
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
+function ContentBlock({ content, className, id }) {
+  return (
+    <div
+      className={className}
+      id={id}
+      dangerouslySetInnerHTML={{ __html: documentToHtmlString(content) }}
+    ></div>
+  );
+}
+
+export default ContentBlock;
