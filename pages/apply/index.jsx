722b64d899ec00be01072252fec157d9bd467b17
jessica6080 <jyan6080@gmail.com>
2023-08-26 18:09:59 -0500

    fix gradient banner

--- pages/apply/index.jsx
+++ pages/apply/index.jsx
@@ -6,12 +6,22 @@ import InvolveSection from "../../components/involveSection";
 import Section from "../../components/section";
 
 function ApplyPage() {
+  const customStyle = {
+    background: "linear-gradient(white, var(--secondary-seafoam))",
+    padding: "80px 0",
+    paddingTop: "calc(60px + var(--nav-height))",
+    "@media (max-width: 800px)": {
+      padding: "40px 0",
+      paddingTop: "calc(40px + var(--nav-height))",
+    },
+  };
   return (
     <>
       <Head title="Apply" />
       <GradientBanner
         title="Apply"
         subHeadline="We're open to students and nonprofits of all backgrounds, no matter your project or technical expertise."
+        style={customStyle}
         arrow
       />
       <Section>
