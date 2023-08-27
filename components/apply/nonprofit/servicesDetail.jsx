d2be1612972edd4a997c1c45231f977bc978b576
EmilioC1 <ec2736@princeton.edu>
2023-08-26 23:36:44 -0400

    added nonprofit apply page and started to add some FAQs

--- components/apply/nonprofit/servicesDetail.jsx
+++ components/apply/nonprofit/servicesDetail.jsx
@@ -12,7 +12,7 @@ export default function ServicesDetail({ content, title }) {
                 </Row>
 
                 <Row>
-                    <ContentBlock content={content.json} />
+                    <div>{content}</div>
                 </Row>
             </Container>
         </Section>
